2e0ec5cb21e9b4d271955cf6675fe5dc02124a14
github username <github email address>
2020-01-04 17:01:38 -0500

    pause2

--- app/application.rb
+++ app/application.rb
@@ -1,2 +1,15 @@
 require 'rack'
 
+class Application
+
+    def call(env)
+      response = Rack::Response.new
+      now = Time.now
+      if now <= 12
+        resp.write "Good morning"
+      else 
+        resp.write "Good afternoon"
+      end
+      resp.finish
+    end
+end
